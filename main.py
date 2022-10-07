f98d3457f8eacddc7ee5e57df833577c7afbf8da
Gabrielle Liebenberg <gabrielle.liebenberg@gmail.com>
2022-10-07 18:09:57 +0200

    can combine data

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import pandas as pd
+import datetime
 
 restaurant1_raw = pd.read_csv('data/orders/restaurant-1-orders.csv')
-data2 = pd.read_csv('data/london_weather.csv')
+weather_raw = pd.read_csv('data/london_weather.csv')
+
+weather_raw['date'] = pd.to_datetime(weather_raw['date'],format='%Y%m%d')
+weather_raw['date'] = weather_raw['date'].dt.date
+
+restaurant1_raw['Order Date'] = pd.to_datetime(restaurant1_raw['Order Date'],format='%d/%m/%Y %H:%M')
+restaurant1_raw['date'] = restaurant1_raw['Order Date'].dt.date
+#restaurant1_raw['date'] = restaurant1_raw['date'].strftime("%d/%m/%Y")
+#.strftime("%d")
+
+#restaurant1_raw['Order Date'] = restaurant1_raw['Order Date'].strftime("%Y/%m/%d")
+#3/8/2019  8:25:00 PM
 
 
-'''
-output1 = pd.merge(data1, data2,
-                   on = 'LOAN_NO',
+output1 = pd.merge(restaurant1_raw, weather_raw,
+                   on = 'date',
                    how = 'inner')
-'''        
+        
 
-print(data2.head(10))
+print(output1.head(10))
