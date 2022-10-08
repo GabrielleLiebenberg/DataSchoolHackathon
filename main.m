6bb8fdeb803a7dabfe99159622ecb838b896511a
Gabrielle Liebenberg <gabrielle.liebenberg@gmail.com>
2022-10-08 10:07:37 +0200

    added new datasets

--- main.m
+++ main.m
@@ -1,13 +1,17 @@
 pyrunfile('main.py');
+ %opts = detectImportOptions("restaurant1_avg_data.csv");
+% %getvaropts(opts,'date');
+ %opts = setvaropts(opts,'date','DatetimeFormat','yyyy-MM-dd');
+%2015-09-2
 
-restaurant1_data = importfile1("restaurant1_data.csv", [2, Inf]);
-restaurant2_data = importfile1("restaurant2_data.csv", [2, Inf]);
+restaurant1_data = importfileAVG("restaurant1_avg_data.csv", [2, Inf]);
+restaurant2_data = importfileAVG("restaurant2_avg_data.csv", [2, Inf]);
 
-%[trainedModel, validationRMSE] = trainRegressionModel(restaurant1_data)
-[trainedModel, validationRMSE] = trainRegressionModel1(restaurant1_data);
+%train model
+%[trainedModel, validationRMSE] = trainRegressionModel1(restaurant1_data);
 
 %generate prediction
-pred = trainedModel.predictFcn(restaurant1_data);
+%pred = trainedModel.predictFcn(restaurant1_data);
 
-plot3('Item Name', 'date', pred, ".")
-grid on
+% plot3('Item Name', 'date', pred, ".")
+% grid on
--- main.py
+++ main.py
@@ -19,6 +19,8 @@ holidays_raw['date'] = pd.to_datetime(holidays_raw['date'],format='%m/%d/%Y %H:%
 holidays_raw['date'] = holidays_raw['date'].dt.date
 
 
+
+
 restaurant1_temp = pd.merge(restaurant1_raw, weather_raw,
                    on = 'date',
                    how = 'inner')
@@ -35,8 +37,15 @@ restaurant2_complete = pd.merge(restaurant2_temp, holidays_raw,
                                 on = 'date',
                                 how='left')
 
-restaurant1_avg = df.groupby(df['']).aggregate(restaurant1_complete)
+agg_functions = {'Quantity': 'sum', 'Product Price': 'first', 'cloud_cover': 'first', 'sunshine': 'first', 'global_radiation': 'first', 'max_temp': 'first', 'mean_temp': 'first', 'min_temp': 'first', 'precipitation': 'first', 'pressure': 'first', 'snow_depth': 'first', 'isPaidTimeOff': 'first'}
+#								countryOrRegion	holidayName	normalizeHolidayName	
+restaurant1_avg = restaurant1_complete.groupby(['date','Item Name']).aggregate(agg_functions)
+restaurant2_avg = restaurant2_complete.groupby(['date','Item Name']).aggregate(agg_functions)
 
-restaurant1_complete.to_csv('restaurant1_data.csv')
-restaurant2_complete.to_csv('restaurant2_data.csv')
+#restaurant1_avg.to_csv('test.csv')
+
+restaurant1_avg.to_csv('restaurant1_avg_data.csv')
+restaurant2_avg.to_csv('restaurant2_avg_data.csv')
+
+#print(restaurant1_avg.head(10))
 
